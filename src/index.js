d968429db17ec89d95d8e35202de503b4ed8988b
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-06-25 12:16:32 +0100

    create routes for all controllers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import 'dotenv/config';
 import cors from 'cors';
 
 import db from './models';
+import locationHandler from './routes/locations';
 
 db.makeConnection();
-
 const app = express();
-
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
-});
+// Parse incoming requests data.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/v1/locations/', locationHandler);
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Example app listening on port ${process.env.PORT || 3000}`),
